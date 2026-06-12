f87e6fffd26eb86d97656b72e5325e2bfc5bfbd5
AlexanderRoFer <fernandex1015@gmail.com>
2026-06-12 08:55:54 -0500

    Initial commit

---
+++ playwright.config.ts
@@ -0,0 +1,99 @@
+import { defineConfig, devices } from '@playwright/test';
+
+/**
+ * Read environment variables from file.
+ * https://github.com/motdotla/dotenv
+ */
+// import dotenv from 'dotenv';
+// import path from 'path';
+// dotenv.config({ path: path.resolve(__dirname, '.env') });
+
+/**
+ * See https://playwright.dev/docs/test-configuration.
+ */
+export default defineConfig({
+  testDir: './tests',
+  /* Run tests in files in parallel */
+  fullyParallel: true,
+  /* Fail the build on CI if you accidentally left test.only in the source code. */
+  forbidOnly: !!process.env.CI,
+  /* Retry on CI only */
+  retries: process.env.CI ? 2 : 0,
+  /* Opt out of parallel tests on CI. */
+  workers: process.env.CI ? 1 : undefined,
+  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
+  reporter: 'html',
+  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+  
+  
+  use: 
+  {
+    //CONFIGURACIONES//
+  /*config para capturar screenshot solo en caso de falla, esto es util para tener evidencia visual de lo que paso en caso de que una prueba falle. */
+    screenshot: 'only-on-failure',
+
+    // headless: false, // se agrega esta linea para que el navegador se abra en modo no headless, esto es util para ver la ejecucion de las pruebas de manera visual.
+  headless: false,
+    // se agrega esta linea para que el navegador ignore los errores de certificados SSL, esto es util cuando se trabaja con entornos de desarrollo o pruebas que no tienen certificados validos.
+  ignoreHTTPSErrors: true,
+
+
+    /* Base URL to use in actions like `await page.goto('')`. */
+    // baseURL: 'http://localhost:3000',
+
+    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+    trace: 'on-first-retry',
+  },
+
+  /* Configure projects for major browsers */
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+
+    /*comentado para no ejecutar en firefox y chrome, solo en google chrome*{
+      name: 'firefox',
+      use: { ...devices['Desktop Firefox'] },
+    },
+
+    //{
+    {
+      name: 'Google Chrome',
+      use: 
+
+      {
+        channel: 'chrome',
+
+      },
+}
+    //},
+
+    /* Test against mobile viewports. */
+    // {
+    //   name: 'Mobile Chrome',
+    //   use: { ...devices['Pixel 5'] },
+    // },
+    // {
+    //   name: 'Mobile Safari',
+    //   use: { ...devices['iPhone 12'] },
+    // },
+
+    /* Test against branded browsers. */
+    // {
+    //   name: 'Microsoft Edge',
+    //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
+    // },
+    // {
+    //   name: 'Google Chrome',
+    //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
+    // },
+  ],
+
+  /* Run your local dev server before starting the tests */
+  // webServer: {
+  //   command: 'npm run start',
+  //   url: 'http://localhost:3000',
+  //   reuseExistingServer: !process.env.CI,
+  // },
+});
